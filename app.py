e7d6c69ea10a30e77bf8f3792208f06b2e10b37e
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-25 16:02:35 +0100

    estrutura do projecto

---
+++ app.py
@@ -0,0 +1,11 @@
+import pyautogui
+from time import sleep
+
+# clicar e digitar meu usuario
+# clicar e digitar minha senha
+# clicar en entrar
+# extrair cada produto
+    # clicar e digitar produto
+    # clicar e digitar a quantidade
+    # clicar e digitar o preco
+    # clicar en registrar
