b192795eb22f92f230d253456b3a31c35601b188
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-25 16:23:09 +0100

    finished

--- app.py
+++ app.py
@@ -13,7 +13,23 @@ pyautogui.write('#netconnect123')
 pyautogui.click(1190,595, duration=2)
 
 # extrair cada produto
+with open('Produt.txt', 'r') as arquivo:
+    for linha in arquivo:
+        produto = linha.split(',')[0]
+        quantidade = linha.split(',')[1]
+        preco = linha.split(',')[2]
+
     # clicar e digitar produto
+        pyautogui.click(1047,530, duration=2)
+        pyautogui.write(produto)
+
     # clicar e digitar a quantidade
+        pyautogui.click(1034,554, duration=2)
+        pyautogui.write(quantidade)
+
     # clicar e digitar o preco
+        pyautogui.click(1034,580, duration=2)
+        pyautogui.write(preco)
     # clicar en registrar
+        pyautogui.click(907,734, duration=1)
+        sleep(1)
