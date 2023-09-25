1b5a96c79d2bb4d2a35860d822665e6bb03f28ca
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-25 16:05:50 +0100

    login finished

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ import pyautogui
 from time import sleep
 
 # clicar e digitar meu usuario
+pyautogui.click(1295,542, duration=2)
+pyautogui.write('lussatisantos')
+
 # clicar e digitar minha senha
+pyautogui.click(1296,568, duration=2)
+pyautogui.write('#netconnect123')
+
 # clicar en entrar
+pyautogui.click(1190,595, duration=2)
+
 # extrair cada produto
     # clicar e digitar produto
     # clicar e digitar a quantidade
